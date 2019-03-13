5d3f6b2073be65c7876860da2206250321b6a515
Antonio Salgado <antoniosalgado@Antonios-MacBook-Pro.local>
2019-03-13 10:53:52 -0300

    added assets to the assets folder

--- original/src/main.js
+++ original/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(BootstrapVue)
 
